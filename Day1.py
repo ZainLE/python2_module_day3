5b3adbb0fae9fa94b9603bfcb8cb04a03ebfa1dc
ZainLatif7164 <latifzain40@gmail.com>
2023-11-27 15:32:14 +0100

    initial version of project

---
+++ Day1.py
@@ -0,0 +1,14 @@
+import time
+import numpy as np
+
+start_time = time.time()
+
+# generate two big matrices of float32
+a = np.random.rand(10000, 10000).astype(np.float32)
+b = np.random.rand(10000, 10000).astype(np.float32)
+
+test = np.matmul(a, b)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+print(test[0:10, 0:10])
