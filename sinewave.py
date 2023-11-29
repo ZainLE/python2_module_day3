d774166f7142770c1696f1a4af7f14a1d8da439e
ZainLatif7164 <latifzain40@gmail.com>
2023-11-29 19:14:22 +0100

    Co-authored-by: Hesam Zakeri <hesamzkr@users.noreply.github.com>

--- sinewave.py
+++ sinewave.py
@@ -2,22 +2,45 @@
 import pygame
 import math
 
-# Setting up 
 
 pygame.init()
-width, height = 1700, 1000
-screen = pygame.display.set_mode((1700, 1000))
+width, height = 1280, 720
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-# black = (0, 0, 0)
-# blue = (0, 0, 255)
-# white= (255,255,255)
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+black = (0, 0, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
+amplitude = 100
+frequency = 0.1
+direction = 1
 center = (width // 2, height // 2)
+player_pos = [center[0], center[1]]
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-pygame.draw.circle(screen, "red", player_pos, 40)
-screen.fill("white")
+
+    screen.fill(black)
+
+    if player_pos[0] > width or player_pos[0] < 0:
+        direction *= -1
+        
+    player_pos[0] += direction
+    player_pos[1] = center[1] + amplitude * math.sin(frequency * player_pos[0])
+    
+    pygame.draw.circle(screen, blue, (player_pos[0], player_pos[1]), 15)
+
+    # pygame.draw.line(screen, red, center, (player_pos[0], player_pos[1]), 3) 
+    
+    
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+
+pygame.quit()
